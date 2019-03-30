71e0aa45e7b575a2b52fb692e87c136a1a6e50f4
Deepika <gotlad1@hawkmail.newpaltz.edu>
2019-03-30 12:43:01 -0400

    updated exercise.js and index.js

--- controllers/exercise.js
+++ controllers/exercise.js
@@ -57,3 +57,304 @@ exports.getExercise = function (req, res) {
     }
   });
 }
+
+exports.createRoutine = function (req, res) {
+  const query = sql = 'INSERT INTO routines SET ?';
+  connection.query(query, req.body, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(result);
+    }
+  });
+}
+exports.getAllRoutines = function (req, res) {
+  var sql = 'SELECT * FROM `routines`';
+  connection.query(sql, function (err, result) {
+    if (err) {
+      console.log(err);
+      res.json(err)
+    } else {
+      res.json({
+        routines: result
+      });
+    }
+  });
+}
+exports.updateRoutine = function (req, res) {
+  const query = 'UPDATE routines SET ? WHERE id = ?'
+  connection.query(query, [req.body, req.query.id], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(result);
+    }
+  });
+}
+exports.removeRoutine = function (req, res) {
+  const query = 'DELETE FROM routines WHERE id = ?'
+  connection.query(query, [req.query.id], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(result);
+    }
+  });
+}
+exports.getRoutine = function (req, res) {
+  const sql = 'SELECT * FROM routines WHERE id=?';
+  connection.query(sql, [req.query.id], function (err, result) {
+    if (err) {
+      console.log(err);
+      res.json(err)
+    } else {
+      res.json({
+        routine: result
+      });
+    }
+  });
+}
+
+
+exports.createWorkout = function (req, res) {
+  const query = sql = 'INSERT INTO workouts SET ?';
+  connection.query(query, req.body, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(result);
+    }
+  });
+}
+exports.getAllWorkouts = function (req, res) {
+  var sql = 'SELECT * FROM `workouts`';
+  connection.query(sql, function (err, result) {
+    if (err) {
+      console.log(err);
+      res.json(err)
+    } else {
+      res.json({
+        workouts: result
+      });
+    }
+  });
+}
+exports.updateWorkout = function (req, res) {
+  const query = 'UPDATE workouts SET ? WHERE id = ?'
+  connection.query(query, [req.body, req.query.id], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(result);
+    }
+  });
+}
+exports.removeWorkout = function (req, res) {
+  const query = 'DELETE FROM workouts WHERE id = ?'
+  connection.query(query, [req.query.id], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(result);
+    }
+  });
+}
+exports.getWorkout = function (req, res) {
+  const sql = 'SELECT * FROM workouts WHERE id=?';
+  connection.query(sql, [req.query.id], function (err, result) {
+    if (err) {
+      console.log(err);
+      res.json(err)
+    } else {
+      res.json({
+        workout: result
+      });
+    }
+  });
+}
+
+exports.createDietplan = function (req, res) {
+  const query = sql = 'INSERT INTO diet_plans SET ?';
+  connection.query(query, req.body, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(result);
+    }
+  });
+}
+exports.getAllDietplans = function (req, res) {
+  var sql = 'SELECT * FROM `diet_plans`';
+  connection.query(sql, function (err, result) {
+    if (err) {
+      console.log(err);
+      res.json(err)
+    } else {
+      res.json({
+        dietPlans: result
+      });
+    }
+  });
+}
+exports.updateDietplan = function (req, res) {
+  const query = 'UPDATE diet_plans SET ? WHERE id = ?'
+  connection.query(query, [req.body, req.query.id], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(result);
+    }
+  });
+}
+exports.removeDietplan = function (req, res) {
+  const query = 'DELETE FROM diet_plans WHERE id = ?'
+  connection.query(query, [req.query.id], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(result);
+    }
+  });
+}
+exports.getDietplan = function (req, res) {
+  const sql = 'SELECT * FROM diet_plans WHERE id=?';
+  connection.query(sql, [req.query.id], function (err, result) {
+    if (err) {
+      console.log(err);
+      res.json(err)
+    } else {
+      res.json({
+        dietPlan: result
+      });
+    }
+  });
+}
+
+exports.createBodytype = function (req, res) {
+  const query = sql = 'INSERT INTO body_types SET ?';
+  connection.query(query, req.body, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(result);
+    }
+  });
+}
+exports.getAllBodytypes = function (req, res) {
+  var sql = 'SELECT * FROM `body_types`';
+  connection.query(sql, function (err, result) {
+    if (err) {
+      console.log(err);
+      res.json(err)
+    } else {
+      res.json({
+        bodyTypes: result
+      });
+    }
+  });
+}
+exports.updateBodytype = function (req, res) {
+  const query = 'UPDATE body_types SET ? WHERE id = ?'
+  connection.query(query, [req.body, req.query.id], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(result);
+    }
+  });
+}
+exports.removeBodytype = function (req, res) {
+  const query = 'DELETE FROM body_types WHERE id = ?'
+  connection.query(query, [req.query.id], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(result);
+    }
+  });
+}
+exports.getBodytype = function (req, res) {
+  const sql = 'SELECT * FROM body_types WHERE id=?';
+  connection.query(sql, [req.query.id], function (err, result) {
+    if (err) {
+      console.log(err);
+      res.json(err)
+    } else {
+      res.json({
+        bodyType: result
+      });
+    }
+  });
+}
+
+exports.createProduct = function (req, res) {
+  const query = sql = 'INSERT INTO products SET ?';
+  connection.query(query, req.body, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(result);
+    }
+  });
+}
+exports.getAllProducts = function (req, res) {
+  var sql = 'SELECT * FROM `products`';
+  connection.query(sql, function (err, result) {
+    if (err) {
+      console.log(err);
+      res.json(err)
+    } else {
+      res.json({
+        products: result
+      });
+    }
+  });
+}
+exports.updateProduct = function (req, res) {
+  const query = 'UPDATE products SET ? WHERE id = ?'
+  connection.query(query, [req.body, req.query.id], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(result);
+    }
+  });
+}
+exports.removeProduct = function (req, res) {
+  const query = 'DELETE FROM products WHERE id = ?'
+  connection.query(query, [req.query.id], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(result);
+    }
+  });
+}
+exports.getProduct = function (req, res) {
+  const sql = 'SELECT * FROM products WHERE id=?';
+  connection.query(sql, [req.query.id], function (err, result) {
+    if (err) {
+      console.log(err);
+      res.json(err)
+    } else {
+      res.json({
+        product: result
+      });
+    }
+  });
+}
--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,34 @@ router.put('/friend', friendCtrl.updateFriend);
 router.delete('/friend', friendCtrl.removeFriend);
 router.get('/friend', friendCtrl.getFriend);
 
+router.post('/exercise/routine', exerciseCtrl.createRoutine);
+router.get('/exercise/routines', exerciseCtrl.getAllRoutines);
+router.put('/exercise/routine', exerciseCtrl.updateRoutine);
+router.delete('/exercise/routine', exerciseCtrl.removeRoutine);
+router.get('/exercise/routine', exerciseCtrl.getRoutine);
+
+router.post('/workout', exerciseCtrl.createWorkout);
+router.get('/workouts', exerciseCtrl.getAllWorkouts);
+router.put('/workout', exerciseCtrl.updateWorkout);
+router.delete('/workout', exerciseCtrl.removeWorkout);
+router.get('/workout', exerciseCtrl.getWorkout);
+
+router.post('/dietplan', exerciseCtrl.createDietplan);
+router.get('/dietplans', exerciseCtrl.getAllDietplans);
+router.put('/dietplan', exerciseCtrl.updateDietplan);
+router.delete('/dietplan', exerciseCtrl.removeDietplan);
+router.get('/dietplan', exerciseCtrl.getDietplan);
+
+router.post('/bodytype', exerciseCtrl.createBodytype);
+router.get('/bodytypes', exerciseCtrl.getAllBodytypes);
+router.put('/bodytype', exerciseCtrl.updateBodytype);
+router.delete('/bodytype', exerciseCtrl.removeBodytype);
+router.get('/bodytype', exerciseCtrl.getBodytype);
+
+router.post('/product', exerciseCtrl.createProduct);
+router.get('/products', exerciseCtrl.getAllProducts);
+router.put('/product', exerciseCtrl.updateProduct);
+router.delete('/product', exerciseCtrl.removeProduct);
+router.get('/product', exerciseCtrl.getProduct);
+
 module.exports = router;
