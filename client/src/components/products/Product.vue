07920ad59a9e5a768937e3abd73b6973a6d8a66c
Deepika <gotlad1@hawkmail.newpaltz.edu>
2019-04-29 23:11:22 -0400

    updated products tab

--- client/src/components/products/Product.vue
+++ client/src/components/products/Product.vue
@@ -1,7 +1,33 @@
 <template>
-  <div>
-    <div class="row ">
-      Products Veiw...
+ <div class="col-lg-12">
+  <div class="" style="text-align: center;margin-bottom:20px">
+    <img src="../../assets/products/1.png" alt="" alt="Snow" style="">
+    <br/><strong>HI-TECH PHARMACEUTICALS OFF THE CHAIN®2$24.50 $34.95</strong>
+  </div>
+  <div class="" style="text-align: center;margin-bottom:20px">
+    <img src="../../assets/products/2.png" alt="" alt="Forest" style="">
+        <br/><strong>
+HI-TECH PHARMACEUTICALS JACK'D UP™5$34.98 $59.95 <br/>
+APS NUTRITION HYDROMAX 180 TABS1$29.95 $39.95</strong>
+  </div>
+  <div class="" style="text-align: center;margin-bottom:20px">
+    <img src="../../assets/products/3.png" alt="" alt="Mountains" style="">
+        <br/><strong>GASPARI NUTRITION SUPER PUMP MAX4$34.95 $64.95</strong>
+  </div>
+    <div class="" style="text-align: center;margin-bottom:20px">
+    <img src="../../assets/products/4.png" alt="" alt="Snow" style="">
+    <br/><strong>BSN NO-XPLODE 60 SERVINGS$52.99 $92.29</strong>
+  </div>
+  <div class="" style="text-align: center;margin-bottom:20px">
+    <img src="../../assets/products/5.png" alt="" alt="Forest" style="">
+    <br/><strong>INSANE LABZ I AM GOD 25 SERVINGS$34.99</strong>
+  </div>
+  <div class="" style="text-align: center;margin-bottom:20px">
+    <img src="../../assets/products/6.png" alt="" alt="Mountains" style="">
+      <br/><strong>5% NUTRITION FULL AS F*CK 30 SERVINGS1$35.95 </strong>
+  </div>
+  <div class="" style="text-align: center;margin-bottom:20px">
+    <img src="../../assets/products/7.png" alt="" alt="Mountains" style="">
   </div>
 </div>
 </template>
@@ -25,5 +51,20 @@ export default {
 
 </script>
 <style>
+* {
+  box-sizing: border-box;
+}
 
+. {
+  float: left;
+  width: 33.33%;
+  padding: 5px;
+  margin-bottom:20px;
+}
+
+.row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
 </style>
